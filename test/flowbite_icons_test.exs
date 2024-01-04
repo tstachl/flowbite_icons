94a8b67964b2bb0ff2eb2fb33dade4331a254b4a
Shibito <ultimaleo@gmail.com>
2024-01-04 21:02:37 +0800

    Updated SVG FIles - Added UnitTest

--- test/flowbite_icons_test.exs
+++ test/flowbite_icons_test.exs
@@ -22,6 +22,12 @@ defmodule FlowbiteIconsTest do
 
     assert render_component(&Flowbite.Icons.outline_angle_down/1, %{sr: "Go Down"}) ==
              ~s|<svg fill="none" viewbox="0 0 14 9" xmlns="http://www.w3.org/2000/svg">\n  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1.497 5.326 5.402a.94.94 0 0 0 .305.21.952.952 0 0 0 1.043-.21L13 1.497">\n  </path>\n</svg>\n<span class="sr-only">Go Down</span>|
+
+    assert render_component(&Flowbite.Icons.outline_redo/1) ==
+             ~s|<svg fill=\"none\" height=\"14\" viewbox=\"0 0 16 14\" width=\"16\" xmlns=\"http://www.w3.org/2000/svg\">\n  <path stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M4 7 1 4l3-3m0 12h6.5a4.5 4.5 0 1 0 0-9H2\">\n  </path>\n</svg>\n<span class=\"sr-only\">Outline Redo icon</span>|
+
+    assert render_component(&Flowbite.Icons.outline_undo/1) ==
+             ~s|<svg fill=\"none\" height=\"14\" viewbox=\"0 0 16 14\" width=\"16\" xmlns=\"http://www.w3.org/2000/svg\">\n  <path stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"m12 7 3-3-3-3m0 12H5.5a4.5 4.5 0 1 1 0-9H14\">\n  </path>\n</svg>\n<span class=\"sr-only\">Outline Undo icon</span>|
   end
 
   test "generated docs" do
